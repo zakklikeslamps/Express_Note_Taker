400fe039060e664a851f186990b4196665512eaf
Zakk Haug <zakklikeslamps@gmail.com>
2021-05-13 15:40:39 -0400

    Still Issues: fixing later

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,49 @@
 //Loads Data
+//const express = require('express');
 const fs = require("fs");
 const path = require("path");
-let notes = require("../db/db.json");
+const db = __dirname + "/../db/db.json";
+const useDb = require("../db/db.json");
+const { v4: uuidv4 } = require('uuid');
 
 //Get/Post
 module.exports = (app) => {
-    
-    app.get('/api/notes', (req, res) =>{
-        res.json(notes)
-    });
 
-    app.post('/api/notes', (req, res) => {
-        const newNote = req.body;
-        console.log(newNote);
-        notes.push(newNote)
-        fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), (err, data) => {
-            if(err) throw err;
-            return res.json(notes);
-        })
+    let notes = JSON.parse(fs.readFileSync(db, "utf8"))
+        //Get request; shows note data
+        app.get("/api/notes", (req, res) => {
 
-    });
+            fs.readFile(db, "utf-8", (err, data) => {
+                if (err) throw err;
+                res.json(JSON.parse(data))
+            })
+        });
 
-}
+        //Post request
+        app.post("api/notes", (req, res) => {
+            const newNote = req.body;
+            req.bodyid = uuidv4();
+            useDb.push(newNote);
+            console.log(useDb);
+
+            fs.writeFile(path.join(_dirname, "../db/db.json"), JSON.stringify(useDb), (err) => {
+                if(err) throw err;
+                res.json("response");
+            });
+        });
+
+        //Delete route
+        app.delete("api/notes/:id", (req, res) => {
+            let idDel = req.params.id;
+
+            notes = notes.filter(noteSelected => {
+                return noteSelected.id !=idDel
+            });
+
+            fs.writeFile(path.join(_dirname, "../db/db.json"), JSON.stringify(notes), (err) => {
+                if (err) throw err;
+                res.json(notes);
+            });
+        });
+   
+};
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 //Dependencies 
 const express = require('express');
 const path = require('path');
+const morgan = require("morgan");
 const app = express();
+
+//Access PORT
 const PORT = process.env.PORT || 3000;
 
 //Sets Express to manage data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan("dev"));
 app.use(express.static("public"));
 
 //Router
